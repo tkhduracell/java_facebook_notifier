aa938f39166321c6d858ff93d5bb7a64526fb808
Filip Lindqvist <buggfille@gmail.com>
2011-08-28 20:05:28 +0100

    Made constant static

--- src/facebooknotifyer/FacebookChecker.java
+++ src/facebooknotifyer/FacebookChecker.java
@@ -27,7 +27,7 @@ public class FacebookChecker {
 
     private final ArrayList<FacebookAccount> accounts;
     private final ArrayList<Notifyer> notfiers;
-    private final int DELAY = 10000;
+    private static final int DELAY = 10000;
     private final Timer timer;
 
     public FacebookChecker(ArrayList<FacebookAccount> accounts, Notifyer notfier) {
